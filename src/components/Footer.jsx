97758360ad772722a899fe01cd0569bb539ecda5
Erin Czarnecki <erin.czarnecki@Erins-MacBook-Pro.local>
2022-09-20 15:39:12 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,24 @@ const Footer = () => {
     return (
         <div>
             <footer className="p-4 sm:p-6 bg-[#0a192f]">
-                <hr className="mb-6 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <hr className="mb-6 sm:mx-auto border-gray-700 lg:my-8" />
                 <div>
                     <div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold uppercase dark:text-pink-600 text-center">Connect with Me</h2>
-                            <ul className=" dark:text-gray-300 flex flex-row justify-between items-center">
+                            <h2 className="flex flex-row justify-center items-center mb-2 text-sm font-semibold uppercase text-pink-500 text-center">Connect with Me</h2>
+                            <ul className="text-gray-300 flex flex-row justify-between items-center">
 
                                 <li>
-                                    <a href="https://github.com/erinczarnecki" rel="noreferrer" target="_blank" className="hover:underline ">Github</a>
+                                    <a className="text-gray-300  hover:text-pink-600 hover:font-bold"
+                                        href="https://github.com/erinczarnecki" rel="noreferrer" target="_blank" >Github</a>
                                 </li>
                                 <li>
-                                    <a href="https://linkedin.com/in/erin-czarnecki" rel="noreferrer" target="_blank" className="hover:underline">LinkedIn</a>
+                                    <a className="text-gray-300  hover:font-bold hover:text-pink-600"
+                                        href="https://linkedin.com/in/erin-czarnecki" rel="noreferrer" target="_blank" >LinkedIn</a>
                                 </li>
 
                                 <li>
-                                    <a
+                                    <a className="text-gray-300  hover:font-bold hover:text-pink-600"
                                         href="mailto:erinczarnecki1@gmail.com">
                                         Email
                         </a>
@@ -27,7 +29,7 @@ const Footer = () => {
 
                                 {/* RESUME */}
                                 <li>
-                                    <a
+                                    <a className="text-gray-300  hover:font-bold hover:text-pink-600"
                                         href="https://drive.google.com/file/d/129DVSph6TXsoDMT1MCoYnLBCq6Z6Uge2/view?usp=sharing" target="_blank" rel="noreferrer">
                                         Resume
                         </a>
@@ -40,7 +42,7 @@ const Footer = () => {
                 </div>
 
 
-                <hr className="my-6 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 Erin Czarnecki - All Rights Reserved.</span>
                 </div>
